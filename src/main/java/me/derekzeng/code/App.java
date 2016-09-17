27d8ac6c4714e072adc89d22734b5e168c9efa3b
coderek <dzeng@rubiconproject.com>
2016-09-17 14:15:23 -0400

    Init

---
+++ src/main/java/me/derekzeng/code/App.java
@@ -0,0 +1,13 @@
+package me.derekzeng.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
